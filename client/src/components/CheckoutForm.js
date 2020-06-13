059b1f460a2b119a68dbca46c5f53fc426b1bb09
Jose Paquian <alostnight1993@outlook.com>
2020-06-12 19:03:28 -0500

    plant list is rendering and items are being added to the cart.

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -13,13 +14,9 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
+const CheckoutForm = props => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const [ values, handleChanges ] = useForm(initialValue);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -66,7 +63,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button className="submitBtn" id= "submit">Checkout</button>
       </form>
 
       {showSuccessMessage && (
